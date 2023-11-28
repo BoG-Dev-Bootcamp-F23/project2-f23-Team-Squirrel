a8b408859962f9ced91517d2e0f41615c48a5d7b
Armin Moinian <44126226+AMoinian2004@users.noreply.github.com>
2023-11-28 17:22:59 -0500

    add change operation to training.js

--- src/pages/api/training.js
+++ src/pages/api/training.js
@@ -1,12 +1,20 @@
-import createTraining from "../../../server/mongodb/actions/createTraining.js"
+import createTraining from "../../../server/mongodb/actions/createTraining.js";
+import updateTraining from "../../../server/mongodb/actions/updateTraining.js";
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
+    if (req.method === "POST") {
         try {
-            await createTraining(req.body)
+            await createTraining(req.body);
+            return res.status(200).send("Success");
         } catch (e) {
-            return res.status(500).send("Failed")
+            return res.status(500).send("Error");
         }
-        return res.status(200).send("Success")
-    }
+    }  else if (req.method === "PATCH") {
+        try {
+            const newTrainingLog = await updateTraining(req.body);
+            return res.status(200).send(newTrainingLog);
+        } catch (e) {
+            return res.status(500).send("Error");
+        }
+    } 
 }
